4593c5783b480c2772a64998708580643b11ec14
Gerson Reis <gersonluareis13@gmail.com>
2020-07-05 13:52:10 +0100

    BenchmarkDotNet
    Installing BenchmarkDotNet

---
+++ TheDevTrack.ObjectToCsv.Client/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheDevTrack.ObjectToCsv.Client
+{
+    public class Car
+    {
+        public Car(int id, string licensePlate, string color, short year)
+        {
+            Id = id;
+            LicensePlate = licensePlate;
+            Color = color;
+            Year = year;
+        }
+
+        [Column("Car code")]
+        public int Id { get; set; }
+        [Column("License plate")]
+        public string LicensePlate { get; set; }
+        [Column("Color")]
+        public string Color { get; set; }
+        [Column("Year")]
+        public short Year { get; set; }
+    }
+}
---
+++ TheDevTrack.ObjectToCsv.Client/DataTableMonitor.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace TheDevTrack.ObjectToCsv.Client
+{
+    [MemoryDiagnoser]
+    public class DataTableMonitor
+    {
+        [Benchmark]
+        public bool TestCreatingCsvString()
+        {
+               Car[] cars = new Car[10]
+               {
+                new Car(1, "52-XX-53", "red", 2020),
+                new Car(1, "25-RR-66", "blue", 2015),
+                new Car(1, "11-FF-98", "orange", 2014),
+                new Car(1, "57-HH-35", "black", 2015),
+                new Car(1, "85-NN-00", "orange", 2019),
+                new Car(1, "35-VV-98", "red", 2010),
+                new Car(1, "09-UU-85", "blue", 2019),
+                new Car(1, "56-QQ-23", "blue", 2016),
+                new Car(1, "00-DD-11", "red", 2012),
+                new Car(1, "11-MM-25", "black", 2017)
+               };
+
+            var dataTable = new DateTable<Car>(cars);
+            var result = dataTable.ToStringCsv();
+            return true;
+        }
+    }
+}
